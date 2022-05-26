8a6da1ccafa721e66a0bd8a5af1d3fb1fd113753
Quentin Bisson <quentin@giantswarm.io>
2022-05-26 11:37:18 +0200

    Add service and pod monitor relabeling back (#7)
    * Add service and pod monitor relabeling back
    
    * Address reviews

--- helm/kyverno-policies-observability/tests/ats/test_common_default.py
+++ helm/kyverno-policies-observability/tests/ats/test_common_default.py
@@ -21,6 +21,10 @@ from ensure import kubeadm_control_plane
 from ensure import kubeadmconfig_controlplane
 from ensure import kubeadmconfig_with_files
 from ensure import kubeadmconfig_with_audit_file
+from ensure import podmonitor
+from ensure import silence
+from ensure import silence_with_matchers
+from ensure import servicemonitor
 
 import pytest
 from pytest_kube import forward_requests, wait_for_rollout, app_template
@@ -60,3 +64,77 @@ def test_kubeadmconfig_auditpolicy(kubeadmconfig_with_audit_file) -> None:
     :param kubeadmconfig_with_audit_file: KubeadmConfig CR which includes an existing audit file
     """
     assert len(kubeadmconfig_with_audit_file['spec']['files']) == 1
+
+
+@pytest.mark.smoke
+def test_silence_heartbeat_policy(silence) -> None:
+    """
+    test_silence_heartbeat_policy tests defaulting of an empty Silence to check for the negative Heartbeat matcher.
+    :param silence: Any Silence CR.
+    """
+    matchers = silence['spec']['matchers']
+
+    for matcher in matchers:
+        if matcher['name'] == "alertname":
+            assert (matcher['value'] == "Heartbeat" and not matcher['isRegex'] and not matcher['isEqual'])
+            return
+
+    pytest.fail("Heartbeat matcher is missing")
+
+@pytest.mark.smoke
+def test_silence_heartbeat_policy_with_existing_matchers(silence_with_matchers) -> None:
+    """
+    test_silence_heartbeat_policy tests defaulting of a Silence container a matcher to check for the negative Heartbeat matcher.
+    :param silence_with_matchers: Any Silence CR container a matcher.
+    """
+    matchers = silence_with_matchers['spec']['matchers']
+
+    assert (matchers[0]['value'] == "test" and matchers[0]['name'] == "test" and not matchers[0]['isRegex'] and not matchers[0]['isEqual'])
+    assert (matchers[1]['value'] == "Heartbeat" and matchers[1]['name'] == "alertname" and not matchers[1]['isRegex'] and not matchers[1]['isEqual'])
+
+
+@pytest.mark.smoke
+def test_pod_monitor_labelling_schema_policy(podmonitor) -> None:
+    """
+    test_service_monitor_labelling_schema_policy tests defaulting of an empty Service monitor to check that the labelling schema is configured.
+    :param podmonitor: Any PodMonitor CR.
+    """
+    endpoints = podmonitor['spec']['podMetricsEndpoints']
+    for endpoint in endpoints:
+        relabelings = endpoint['relabelings']
+        assert relabelings[0]['replacement'] == '' and relabelings[0]['targetLabel'] == 'cluster_id'                                      \
+          and relabelings[1]['replacement'] == 'management_cluster' and relabelings[1]['targetLabel'] == 'cluster_type'                   \
+          and relabelings[2]['replacement'] == '' and relabelings[2]['targetLabel'] == 'provider'                                         \
+          and relabelings[3]['replacement'] == '' and relabelings[3]['targetLabel'] == 'installation'                                     \
+          and relabelings[4]['sourceLabels'] == ['__meta_kubernetes_namespace'] and relabelings[4]['targetLabel'] == 'namespace'          \
+          and relabelings[5]['sourceLabels'] == ['__meta_kubernetes_pod_name'] and relabelings[5]['targetLabel'] == 'pod'                 \
+          and relabelings[6]['sourceLabels'] == ['__meta_kubernetes_pod_container_name'] and relabelings[6]['targetLabel'] == 'container' \
+          and relabelings[7]['sourceLabels'] == ['__meta_kubernetes_pod_node_name'] and relabelings[7]['targetLabel'] == 'node'           \
+          and relabelings[8]['sourceLabels'] == ['__meta_kubernetes_node_label_role'] and relabelings[8]['targetLabel'] == 'role'         \
+          and relabelings[9]['replacement'] == '' and relabelings[9]['targetLabel'] == 'customer'                                         \
+          and relabelings[10]['replacement'] == 'default' and relabelings[10]['targetLabel'] == 'organization'                                   \
+          and relabelings[11]['sourceLabels'] == ['organization'] and relabelings[11]['regex'] == 'org-(.*)' and relabelings[11]['replacement'] == '${1}' and relabelings[11]['targetLabel'] == 'organization' \
+        , 'Invalid relabelings {} '.format(relabelings)
+
+@pytest.mark.smoke
+def test_service_monitor_labelling_schema_policy(servicemonitor) -> None:
+    """
+    test_service_monitor_labelling_schema_policy tests defaulting of an empty Service monitor to check that the labelling schema is configured.
+    :param servicemonitor: Any ServiceMonitor CR.
+    """
+    endpoints = servicemonitor['spec']['endpoints']
+    for endpoint in endpoints:
+        relabelings = endpoint['relabelings']
+        assert relabelings[0]['replacement'] == '' and relabelings[0]['targetLabel'] == 'cluster_id'                                      \
+          and relabelings[1]['replacement'] == 'management_cluster' and relabelings[1]['targetLabel'] == 'cluster_type'                   \
+          and relabelings[2]['replacement'] == '' and relabelings[2]['targetLabel'] == 'provider'                                         \
+          and relabelings[3]['replacement'] == '' and relabelings[3]['targetLabel'] == 'installation'                                     \
+          and relabelings[4]['sourceLabels'] == ['__meta_kubernetes_namespace'] and relabelings[4]['targetLabel'] == 'namespace'          \
+          and relabelings[5]['sourceLabels'] == ['__meta_kubernetes_pod_name'] and relabelings[5]['targetLabel'] == 'pod'                 \
+          and relabelings[6]['sourceLabels'] == ['__meta_kubernetes_pod_container_name'] and relabelings[6]['targetLabel'] == 'container' \
+          and relabelings[7]['sourceLabels'] == ['__meta_kubernetes_pod_node_name'] and relabelings[7]['targetLabel'] == 'node'           \
+          and relabelings[8]['sourceLabels'] == ['__meta_kubernetes_node_label_role'] and relabelings[8]['targetLabel'] == 'role'         \
+          and relabelings[9]['replacement'] == '' and relabelings[9]['targetLabel'] == 'customer'                                         \
+          and relabelings[10]['replacement'] == 'default' and relabelings[10]['targetLabel'] == 'organization'                                   \
+          and relabelings[11]['sourceLabels'] == ['organization'] and relabelings[11]['regex'] == 'org-(.*)' and relabelings[11]['replacement'] == '${1}' and relabelings[11]['targetLabel'] == 'organization' \
+        , 'Invalid relabelings {}'.format(relabelings)
