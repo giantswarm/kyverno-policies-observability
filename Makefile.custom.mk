6e4c1d7a8ee2a3ba358e750ae76330518a5609a7
Zirko <64951262+QuantumEnigmaa@users.noreply.github.com>
2024-10-22 17:33:02 +0200

    Apply suggestions from code review
    Co-authored-by: Zach Stone <zach@giantswarm.io>

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -36,7 +36,7 @@ tilt-up: ## Start Tilt
 .PHONY: install-kyverno
 install-kyverno:
 	kubectl create --context kind-$(KIND_CLUSTER_NAME) -f https://github.com/kyverno/kyverno/releases/download/v1.12.5/install.yaml
-	kubectl wait --context kind-$(KIND_CLUSTER_NAME) --for=condition=ready --timeout=90s pod -l app=kyverno -nkyverno
+	kubectl wait --context kind-$(KIND_CLUSTER_NAME) --for=condition=ready --timeout=90s pod -l app.kubernetes.io/instance=kyverno -nkyverno
 
 .PHONY: kind-get-kubeconfig
 kind-get-kubeconfig:
